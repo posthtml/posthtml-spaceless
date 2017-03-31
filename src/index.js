f5d96ccda76f87cc76cc94c74d3f238d2492704f
Ivan Demidov <demidov@220-volt.ru>
2017-03-31 11:01:49 +0300

    fix(index): Remove space only between tags, close #3

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import parser from 'posthtml-parser';
 import render from 'posthtml-render';
 
-const clean = tree => parser(render(tree))
-	.filter(node => {
-		return typeof node === 'object' || (typeof node === 'string' && (node.trim().length !== 0 || /doctype/gi.test(node)));
-	})
+const clean = tree => tree
+	.filter(node => typeof node === 'object' || (typeof node === 'string' && (node.trim().length !== 0 || /doctype/gi.test(node))))
 	.map(node => {
 		if (Object.prototype.hasOwnProperty.call(node, 'content')) {
 			node.content = clean(node.content);
 		}
 
-		return typeof node === 'string' ? node.trim() : node;
+		return node;
 	});
 
 const spaceless = tree => tree.match({tag: 'spaceless'}, node => {
 	node.tag = false;
-	return clean(node);
+	return clean(parser(render(node).trim()));
 });
 
 const cloneClassTo = tree => Promise.resolve(tree).then(tree => spaceless(tree)).then(tree => tree);
